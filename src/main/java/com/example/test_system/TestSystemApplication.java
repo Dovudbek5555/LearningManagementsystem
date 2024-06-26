dff565b0501c302ee8263f135398fd69ca7ae283
Dovudbek5555 <qodirovdovud77@gmail.com>
2024-06-26 19:06:05 +0500

    First Commit

---
+++ src/main/java/com/example/test_system/TestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_system/TestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
