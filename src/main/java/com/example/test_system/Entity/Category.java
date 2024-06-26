51c8e7752baf608c14813caa60ddd666705fff0c
Dovudbek5555 <qodirovdovud77@gmail.com>
2024-06-26 19:22:37 +0500

    Category

---
+++ src/main/java/com/example/test_system/Entity/Category.java
@@ -0,0 +1,24 @@
+package com.example.test_system.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
