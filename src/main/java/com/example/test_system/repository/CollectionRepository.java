c0a04126933d8fa7a9e6a3abbefd20111a7f502a
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-06-27 15:35:00 +0300

    Buldi

--- src/main/java/com/example/test_system/repository/CollectionRepository.java
+++ src/main/java/com/example/test_system/repository/CollectionRepository.java
@@ -1,6 +1,7 @@
 package com.example.test_system.repository;
- import com.example.test_system.Entity.Collection;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CollectionRepository extends JpaRepository<Collection, Integer> {
+ import com.example.test_system.entity.QuestionList;
+ import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollectionRepository extends JpaRepository<QuestionList, Integer> {
 }
