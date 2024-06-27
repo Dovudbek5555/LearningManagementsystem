d1106d9daee9a33e0d1836fb5425f1dff2232429
Dovudbek5555 <qodirovdovud77@gmail.com>
2024-06-27 16:05:19 +0500

    SecurityAndConfigv3

--- src/main/java/com/example/test_system/repository/QuestionListRepository.java
+++ src/main/java/com/example/test_system/repository/QuestionListRepository.java
@@ -3,5 +3,5 @@ package com.example.test_system.repository;
 import com.example.test_system.entity.QuestionList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CollectionRepository extends JpaRepository<QuestionList, Integer> {
+public interface QuestionListRepository extends JpaRepository<QuestionList, Integer> {
 }
