72f48bf826c2050d3554012e86df036713fe534e
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-06-28 16:26:30 +0300

    exam

--- src/main/java/com/example/test_system/controller/ExamController.java
+++ src/main/java/com/example/test_system/controller/ExamController.java
@@ -1,6 +1,12 @@
 package com.example.test_system.controller;
 
+import com.example.test_system.payload.ApiResponse;
+import com.example.test_system.payload.ExamDto;
+import com.example.test_system.service.ExamService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/exam")
 public class ExamController {
-
+    private final ExamService examService;
+    @PostMapping
+    public HttpEntity<ApiResponse> saveExam(@RequestBody ExamDto examDto) {
+        return null;
+    }
 }
