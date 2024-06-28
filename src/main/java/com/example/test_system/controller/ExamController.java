04fec13d35c4e8e16846825f1a7e3cd52f06a28c
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-06-28 16:22:51 +0300

    exam

---
+++ src/main/java/com/example/test_system/controller/ExamController.java
@@ -0,0 +1,12 @@
+package com.example.test_system.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/exam")
+public class ExamController {
+
+}
