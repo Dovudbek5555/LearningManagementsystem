72fdbccca0439c062a5fbbe7f8d5124363674516
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-06-29 14:00:07 +0300

    Question tug'rilandi

--- src/main/java/com/example/test_system/service/QuestionService.java
+++ src/main/java/com/example/test_system/service/QuestionService.java
@@ -2,7 +2,9 @@ package com.example.test_system.service;
 
 import com.example.test_system.entity.Question;
 import com.example.test_system.entity.SubCategory;
+import com.example.test_system.entity.User;
 import com.example.test_system.entity.enums.DifficultyEnum;
+import com.example.test_system.entity.enums.RoleEnum;
 import com.example.test_system.exceptions.GenericException;
 import com.example.test_system.payload.ApiResponse;
 import com.example.test_system.payload.QuestionDto;
@@ -39,18 +41,51 @@ public class QuestionService {
         return new ApiResponse("DifficultyEnum not found", false, HttpStatus.BAD_REQUEST, null);
     }
 
-    public ApiResponse getQuestionList(){
+    public ApiResponse getQuestionList(User user){
         List<QuestionDto> questionDtos = new ArrayList<>();
         for (Question question : questionRepository.findAll()) {
+            if (user.getRoleEnum()== RoleEnum.TEACHER|| user.getRoleEnum()== RoleEnum.ADMIN){
+                QuestionDto questionDto = QuestionDto.builder()
+                        .id(question.getId())
+                        .question(question.getQuestion())
+                        .subCategoryId(question.getSubCategory().getId())
+                        .difficultyEnum(question.getQuestion())
+                        .build();
+                questionDtos.add(questionDto);
+            }
             QuestionDto questionDto = QuestionDto.builder()
                     .id(question.getId())
                     .question(question.getQuestion())
                     .subCategoryId(question.getSubCategory().getId())
-                    .difficultyEnum(question.getQuestion())
                     .build();
             questionDtos.add(questionDto);
+
         }
         return new ApiResponse("Success", true, HttpStatus.OK, questionDtos);
+    }
+
+    public ApiResponse updateQuestion(QuestionDto questionDto){
+        Question question = questionRepository.findById(questionDto.getId())
+                .orElseThrow(() -> GenericException.builder().message("Question not found").statusCode(400).build());
+        boolean existsed = questionRepository.existsByQuestion(question.getQuestion());
+        SubCategory subCategory = subCategoryRepository.findById(questionDto.getSubCategoryId())
+                .orElseThrow(() -> GenericException.builder().message("SubCategory not found").statusCode(400).build());
+        if (!existsed){
+            question.setId(questionDto.getId());
+            question.setQuestion(questionDto.getQuestion());
+            question.setDifficulty(DifficultyEnum.valueOf(questionDto.getDifficultyEnum()));
+            question.setSubCategory(subCategory);
+            questionRepository.save(question);
+            return new ApiResponse("Question successfully updated", true, HttpStatus.OK, null);
+        }
+        return new ApiResponse("Question not found", false, HttpStatus.BAD_REQUEST, null);
+    }
+
+    public ApiResponse deleteQuestion(Integer id){
+        Question question = questionRepository.findById(id)
+                .orElseThrow(() -> GenericException.builder().message("Question not found").statusCode(400).build());
+        questionRepository.delete(question);
+        return new ApiResponse("Successfully deleted",true,HttpStatus.OK,null);
     }
 
 }
