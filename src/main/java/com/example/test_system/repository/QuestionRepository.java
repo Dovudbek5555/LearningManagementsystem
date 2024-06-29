45fe5dc5dc1125b78389cc3ce9169d153e31e766
Quvonch09 <quvonchbekbobomurodov9@gmail.com>
2024-06-29 16:55:58 +0300

    Tugrilandi

--- src/main/java/com/example/test_system/repository/QuestionRepository.java
+++ src/main/java/com/example/test_system/repository/QuestionRepository.java
@@ -3,8 +3,13 @@ package com.example.test_system.repository;
 import com.example.test_system.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     boolean existsByQuestion(String question);
+
+    default List<Question> findAllByDifficulty(String difficulty) {
+        return null;
+    }
 }
